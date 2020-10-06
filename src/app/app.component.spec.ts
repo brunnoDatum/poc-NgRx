fe1bbeee3a6e4320435d1907ff3f060cd134f24c
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2020-10-06 14:25:23 -0400

    Adding NgRx Store

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'poc-NgRx2'`, () => {
+  it(`should have as title 'poc-NgRx'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('poc-NgRx2');
+    expect(app.title).toEqual('poc-NgRx');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('poc-NgRx2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('poc-NgRx app is running!');
   });
 });
